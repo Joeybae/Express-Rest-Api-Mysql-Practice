34dea599e91d4b6a83e52256f0545bf82f47aa50
Joeybae <45925992+Joeybae@users.noreply.github.com>
2019-11-28 15:15:58 +0900

    Update models.js

--- app/models/models.js
+++ app/models/models.js
@@ -1,9 +1,4 @@
 const Sequelize = require('sequelize');
-// const sequelize = new Sequelize('node_api_codelab', 'root', 'Bae8921!',{
-//   host: 'localhost',
-//   dialect: 'mysql'// pick one of 'mysql','sqlite','postgres','mssql',
-// });
-
 const config = require('../config/environments');
 const sequelize = new Sequelize(
   config.mysql.database,
